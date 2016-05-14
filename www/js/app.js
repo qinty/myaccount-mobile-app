221180485ecddbd0cdf037c00d4b5d08501b4d76
Radu Andronescu <radu.andronescu@avangate.com>
2016-05-14 17:32:23 +0300

    settings html

--- www/js/app.js
+++ www/js/app.js
@@ -82,7 +82,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'jett.ionic.filter.ba
                         controller : 'ProductCtrl'
                     }
                 }
-            });
+            })
+            .state('app.settings', {
+              url  : '/settings',
+              views: {
+                'menuContent': {
+                  templateUrl: 'templates/settings.html',
+                  controller : 'SettingsCtrl'
+                }
+              }
+            })
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/app/products');
     });
